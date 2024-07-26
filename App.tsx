89d707194a19b108b9e487f6fc243a0dc6b93f6f
Marcos Koch <marcos_koch@hotmail.com>
2024-07-25 23:39:51 -0300

    configured roboto font

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import { Home } from "@screens/Home";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+  if (!fontsLoaded) {
+    return <View />
+  }
   return (
     <Home />
   );
